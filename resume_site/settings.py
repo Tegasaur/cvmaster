c6f13debdbefc863626132b16402396783823fff
SamuelOkei <samuel.o.okei@ttu.edu>
2020-05-29 01:03:33 -0500

    finalproduct

--- resume_site/settings.py
+++ resume_site/settings.py
@@ -130,8 +130,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 )
