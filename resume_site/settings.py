08dd4aa3e24b75bc8eced12935bbb7b2b26e24f0
SamuelOkei <samuel.o.okei@ttu.edu>
2020-05-29 01:18:23 -0500

    finalproduct

--- resume_site/settings.py
+++ resume_site/settings.py
@@ -129,5 +129,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
